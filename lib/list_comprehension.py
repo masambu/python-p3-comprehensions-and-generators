f6adc34d733d7a9c2c48888b93997781e52a1377
Kelvin Masambu <kevohmasambu@gmail.com>
2024-03-02 13:45:26 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [n for n in num_list if n%2 == 0]
+    return num_list
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list =  [f"{words}!" for words in sentence_list]
+    return sentence_list
+
+print(make_exclamation(["Hello", "World"]))
